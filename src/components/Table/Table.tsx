dc2e2696e0cb64c2c60e5daa5a65d98f2627a04a
Gabriel Spranger <gabriel.spranger@utec.edu.pe>
2023-06-19 05:16:44 +0000

    feat: table done with dummy data

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -5,16 +5,556 @@ type ChartProps = {
   frequencies: number[];
 };
 
+type FakeData = {
+  raceName: string;
+  driverName: string;
+  driverNumber: number;
+  racePosition: number;
+  raceDate: string;
+};
+
+const fakeData: FakeData[] = [
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+  {
+    raceName: 'Aussie Grand Prix',
+    driverName: 'James Hunt',
+    driverNumber: 5,
+    racePosition: 1,
+    raceDate: '1999'
+  },
+];
+
+/* NOTE: 1 <= racePosition <= 10 */
+/* NOTE: Cada constructor tiene dos autos en la competencia en cada carrera */
+
 const Table = (props: ChartProps): JSX.Element => {
   return (
     <div className="tableContainer">
-      <h1>Table</h1>
-      <h1>Table</h1>
-      <h1>Table</h1>
-      <h1>Table</h1>
-      {/* NOTE: Cada constructor tiene dos autos en la competencia en cada carrera */}
-      {/* NOTE: 1 <= racePosition <= 10 */}
-      {/* raceName, driverName, diverNumber, racePosition, raceDate */}
+      <div className="tableHeader">
+        <h2 className="sticky">#</h2>
+        <h2 className="sticky">Race</h2>
+        <h2 className="sticky">Driver</h2>
+        <h2 className="sticky">Driver No</h2>
+        <h2 className="sticky">Position</h2>
+        <h2 className="sticky">Date</h2>
+      </div>
+      <div className="tableData">
+        {fakeData.map((data, i) => {
+          return (
+            <>
+              <p>{i}</p>
+              <p>{data.raceName}</p>
+              <p>{data.driverName}</p>
+              <p>{data.driverNumber}</p>
+              <p>{data.racePosition}</p>
+              <p>{data.raceDate}</p>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 };
