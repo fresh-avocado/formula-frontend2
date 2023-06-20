5ba502e095eca611b3f08b067d65b70938b1d484
Gabriel Spranger <gabriel.spranger@utec.edu.pe>
2023-06-20 21:25:15 +0000

    refactor: changed root path from '/' to '/v1'

--- src/utils/constants/urls.ts
+++ src/utils/constants/urls.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:8080";
+export const baseUrl = "http://localhost:8080/v1";
 export const getConstructorsUrl = baseUrl + "/constructors";
 export const updateFavConstructorUrl = getConstructorsUrl + "/updateFav";
 export const getFavConstructorsUrl = getConstructorsUrl + "/favs";
